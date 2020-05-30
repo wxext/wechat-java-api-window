f44d47edf732ab71268dea3079f99d4b0be449d9
year <nc@abashk.com>
2020-05-30 12:26:32 +0800

    Fixing some path

--- wechat/src/Lin/Wechat/Handler/Event/Interface/EventListener.java
+++
@@ -1,8 +0,0 @@
-package Lin.Wechat.Handler.Event.Interface;
-
-import Lin.Wechat.WXBot.WXBot;
-import cn.hutool.json.JSONObject;
-
-public interface EventListener{
-	public void process(JSONObject result, WXBot bot);
-}
--- wechat/src/Lin/Wechat/Handler/Event/Login/EventLoginAuth.java
+++
@@ -1,9 +0,0 @@
-package Lin.Wechat.Handler.Event.Login;
-
-import Lin.Wechat.WXBot.WXBot;
-import cn.hutool.json.JSONObject;
-
-public class EventLoginAuth {
-	public EventLoginAuth(JSONObject json, WXBot bot) {
-	}
-}
