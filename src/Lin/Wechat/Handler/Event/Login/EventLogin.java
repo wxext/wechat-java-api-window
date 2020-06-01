e21c354444ab79cc1cb55afc3643b207ad8acf32
year <nc@abashk.com>
2020-06-01 13:05:58 +0800

    Auth XT automatically

--- src/Lin/Wechat/Handler/Event/Login/EventLogin.java
+++ src/Lin/Wechat/Handler/Event/Login/EventLogin.java
@@ -1,38 +1,15 @@
 package Lin.Wechat.Handler.Event.Login;
 
-import java.net.URLEncoder;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
-import Lin.Wechat.Sender.BindXTAndWx;
-import Lin.Wechat.Sender.XTAuth;
 import Lin.Wechat.WXBot.WXBot;
 import cn.hutool.json.JSONObject;
 
 public class EventLogin {
-	@SuppressWarnings("deprecation")
 	public EventLogin(JSONObject json, WXBot bot) {
 		System.out.println("登陆成功 ...");
 		bot.getInfo().setLogin(true);
 		bot.getInfo().getJf().dispose();
 		String wxid = new JSONObject(json.getStr("data")).getStr("wxid");
-		String url = "https://api.wxext.cn/auth?";
-		url += "wxid=" + wxid;
-		url += "&";
-		url += "data=" + URLEncoder.encode(bot.getInfo().getMac());
-		JSONObject authResult = new XTAuth(bot).send(url);
-		//System.out.println(url);
-		//System.out.println(authResult);
-		bot.getInfo().setAuthDate(authResult.getDate("auth"));
-		bot.getInfo().setExpireDate(authResult.getDate("expire"));
-		bot.getInfo().setNickName(authResult.getStr("nickName"));
-		new BindXTAndWx(bot).send();		
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-		String str = "获取信息 " + " Auth:";
-		str += df.format(bot.getInfo().getAuthDate());
-		str += " Expire:";
-		str += df.format(bot.getInfo().getExpireDate());
-		System.out.println(str);
-		//System.out.println(json.toString());
+		bot.getInfo().setWxid(wxid);
+		new ExtendsExpireDate(bot);
 	}
 }
---
+++ src/Lin/Wechat/Handler/Event/Login/ExtendsExpireDate.java
@@ -0,0 +1,41 @@
+package Lin.Wechat.Handler.Event.Login;
+
+import java.net.URLEncoder;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import Lin.Wechat.Sender.BindXTAndWx;
+import Lin.Wechat.Sender.XTAuth;
+import Lin.Wechat.WXBot.WXBot;
+import cn.hutool.json.JSONObject;
+
+public class ExtendsExpireDate {
+	WXBot bot;
+
+	public ExtendsExpireDate(WXBot bot) {
+		this.bot = bot;
+
+		String url = "https://api.wxext.cn/auth?";
+		url += "wxid=" + bot.getInfo().getWxid();
+		url += "&";
+		url += "data=" + URLEncoder.encode(bot.getInfo().getMac());
+		JSONObject authResult = new XTAuth(bot).send(url);
+		bot.getInfo().setAuthDate(authResult.getDate("auth"));
+		bot.getInfo().setExpireDate(authResult.getDate("expire"));
+		bot.getInfo().setNickName(authResult.getStr("nickName"));
+		new BindXTAndWx(bot).send();
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		String str = "获取信息 " + " Auth:";
+		str += df.format(bot.getInfo().getAuthDate());
+		str += " Expire:";
+		str += df.format(bot.getInfo().getExpireDate());
+		System.out.println(str);
+		long sleep = bot.getInfo().getExpireDate().getTime() - bot.getInfo().getAuthDate().getTime() - 3600 * 1000;
+		try {
+			Thread.sleep(sleep);
+		} catch (InterruptedException e) {
+		}
+		new ExtendsExpireDate(bot);
+
+	}
+}
