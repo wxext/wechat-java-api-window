202737c25cb3232821941fe3d07be96aca80a0f0
year <nc@abashk.com>
2020-06-01 12:07:01 +0800

    mirror fix

--- src/Test/Listener/MessageHandler.java
+++ src/Test/Listener/MessageHandler.java
@@ -3,100 +3,99 @@ package Test.Listener;
 import Lin.Wechat.Handler.Event.Interface.Event;
 import Lin.Wechat.Handler.Event.Interface.EventHandler;
 
-public class MessageHandler implements EventHandler{
+public class MessageHandler implements EventHandler {
 
 	public void onTextMessage(Event event) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	public void onImgMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onVoiceMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onFriendConfirmMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onVideoMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onEmotionMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onLocationMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onLinkShareMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onGroupMemberInfoUpdateMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onGroupMemberIncreaseMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onGroupMemberDecreaseMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onFriendInfoUpdateMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onReceivePaymentMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onFriendAuthMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onCreateGroupMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onXMLImgPathMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onVoiceCallMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onVoiceCallRejectMessage(Event event) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onVoiceCallAcceptMessage(Event event) {
 		// TODO Auto-generated method stub
-		
-	}	
+
+	}
 }
